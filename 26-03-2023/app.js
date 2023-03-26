d9cf32ddba41d832ae7f20b7026bdea354f58288
Muhammad Usman <usman.ashraf033@gmail.com>
2023-03-26 16:53:49 +0500

    Add files via upload

---
+++ 26-03-2023/app.js
@@ -0,0 +1,50 @@
+// const isStudent = prompt("Are you a student of BQ ? Yes/No");
+
+// if(isStudent === "Yes"){
+//     alert("You are not eligible")
+// }else {
+//     alert("You are eligible")
+// }
+
+// const shop1 = "Open";
+// const shop2 = "Open";
+
+// if (shop1 !== "Closed") {
+//     alert("Buy From Shop 1")
+// } else if (shop2 === "Open") {
+//     alert("Buy From Shop 2")
+// } else {
+//     alert("Buy From Shop 3")
+// }
+
+// const percentage = prompt("Enter Your Percentage");
+
+// if (percentage >= 80) {
+//     alert("A+")
+// } else if (percentage >= 70) {
+//     alert("A")
+// } else if (percentage >= 60) {
+//     alert("B")
+// } else if (percentage >= 50) {
+//     alert("C")
+// } else if (percentage >= 40) {
+//     alert("D")
+// } else {
+//     alert("F")
+// }
+
+// const shop1 = "Open"
+// const shop1Item = "Cola"
+// const shop2 = "Open"
+// const shop2Item = "Chips"
+
+// if (shop1 === "Open" && (shop1Item === "Chips" || shop1Item === "Pastries")) {
+//     alert("Buy " + shop1Item + " from shop 1")
+// } else if (shop1 === "Open" && shop2Item === "Chips") {
+//     alert("Buy chips from shop 2")
+// }
+
+for (let i = 1; i <=10; i++) {
+    console.log("2 X " + i + " = " + 2*i )
+}
+

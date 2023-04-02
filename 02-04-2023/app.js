f8a80793eeb4ba7a2580ab4295ee631c509276f3
Muhammad Usman <usman.ashraf033@gmail.com>
2023-04-02 17:01:04 +0500

    Add files via upload

---
+++ 02-04-2023/app.js
@@ -0,0 +1,45 @@
+// const userCity = prompt("Enter your city name")
+// const cities = ["Karachi", "Lahore", "Islamabad"]
+
+// let isCityFound = false;
+// for (let i = 0; i < cities.length; i++) {
+//     if (userCity.toLowerCase() !== cities[i].toLowerCase()) {
+//         continue;
+//     }
+
+//     isCityFound = true;
+//     alert("Your city is available")
+//     break;
+// }
+
+// if (isCityFound === false) {
+//     alert("City not availale")
+// }
+
+// const nums = [[1, 2, 3, 4], [5, 7, 9, 6], [3, 4, 6, 9]];
+// const output = []
+// for (let i = 0; i < nums.length; i++) {
+//     const element = nums[i];
+//     let sum = 0;
+//     for (let j = 0; j < element.length; j++) {
+//         const number = element[j];
+//         sum = sum + number;
+//     }
+//     output.push(sum)
+// }
+
+// for (let i = 0; i < output.length; i++) {
+//     alert("Sum of index " + i + " is " + output[i])
+// }
+
+
+
+// const students = [["Sajid", 1, 18], ["Ghaffar", 2, 22], ["Ahmed", 3, 24]]
+// const studentRollNo = prompt("Enter student roll no")
+
+// for (let i = 0; i < students.length; i++) {
+//     if (studentRollNo == students[i][1]) {
+//         alert("Student Name is " + students[i][0] + " and age is " + students[i][2])
+//     }
+// }
+

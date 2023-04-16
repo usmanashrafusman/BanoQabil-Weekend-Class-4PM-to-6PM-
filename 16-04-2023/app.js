6dee47ee6944fc5fecc546f09d01f11d3835b1cc
Muhammad Usman <usman.ashraf033@gmail.com>
2023-04-16 16:59:10 +0500

    Add files via upload

---
+++ 16-04-2023/app.js
@@ -0,0 +1,77 @@
+// // // const table = 10;
+
+// // // function printTable() {
+// // //     const table = 20;
+// // //     console.log(table , "Inside Function")
+// // //     for (let i = 1; i <= 10; i++) {
+// // //         // console.log(table + " x " + i + " = " + table * i)
+// // //     }
+// // // }
+
+// // // printTable()
+
+// // // console.log(table , "Outisde Function")
+
+// // function multiply(num1, num2) {
+// //     const multiple = num1 * num2;
+// //     // console.log(multiple)
+// //     return multiple;
+// // }
+
+// // function add(num1, num2) {
+// //     const sum = num1 + num2;
+// //     if (sum > 10) {
+// //         return sum;
+// //     }
+// //     return "Less Than 10"
+// // }
+
+
+// // const num1 = multiply(2, 2);
+// // const num2 = add(num1, 5)
+// // console.log(num2);
+
+// // const dayOfWk = "Fr";
+
+// // switch (dayOfWk) {
+// //     case "Sat":
+// //         alert("Whoopee");
+// //         break;
+// //     case "Sun":
+// //         alert("Whoopee");
+// //         break;
+// //     case "Fri":
+// //         alert("TGIF!");
+// //         break;
+// //     default:
+// //         alert("Shoot me now!");
+// // }
+
+
+// const arrayOfEven = [
+//     2,
+//     4,
+//     6,
+//     8,
+//     10,
+//     12,
+//     14,
+//     16,
+//     18,
+//     20
+// ]
+// let counter = 22;
+// do {
+//     alert("While Loop Running")
+//     if (counter % 2 === 0) {
+//         arrayOfEven.push(counter)
+//     }
+//     ++counter;
+// }
+// while (10 > arrayOfEven.length)
+
+// console.log(arrayOfEven)
+
+function sayHi(){
+    alert("HELLO")
+}
